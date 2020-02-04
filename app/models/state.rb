be345bdd00e5259712caedb2e05cbeb67b9252dc
Student <example@example.com>
2020-02-03 16:47:41 -0800

    Update model.

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,6 @@
 class State < ApplicationRecord
   validates_presence_of :whats_needed, :primary, :absentee, :senators, :name, :code, :voter_id
   belongs_to :voter, optional: true
+  has_one :politician_current
+  accepts_nested_attributes_for :politician_current, :allow_destroy => true
 end
