14ec7f33e3a2f2fe6e6e87427a5d37c13e56f75c
Student <example@example.com>
2020-02-04 14:31:58 -0800

    Update models.

--- app/models/politician_current.rb
+++ app/models/politician_current.rb
@@ -1,4 +1,6 @@
-class PoliticianCurrent < ApplicationRecord
-  validates_presence_of :senators, :representatives
-  belongs_to :state, optional: true
-end
+# class PoliticianCurrent < ApplicationRecord
+#   validates_presence_of :senators, :representatives
+#   belongs_to :state, optional: true
+#   accepts_nested_attributes_for :senator, :allow_destroy => true
+#   has_many :senators
+# end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,5 +1,5 @@
 class State < ApplicationRecord
-  validates_presence_of :whats_needed, :primary, :absentee, :senators, :name, :code, :voter_id
+  validates_presence_of :whats_needed, :primary, :absentee, :name, :code, :voter_id
   belongs_to :voter, optional: true
   has_one :politician_current
   accepts_nested_attributes_for :politician_current, :allow_destroy => true
