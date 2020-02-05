d0d7828a0b18f6cfbe52dab5395b65156bf7f3a1
Student <example@example.com>
2020-02-05 13:38:02 -0800

    Update state controller.

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -46,6 +46,6 @@ class StatesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def state_params
-      params.require(:state).permit(:whats_needed, :primary, :absentee, senator_attributes: [ :id, :name, :phone ])
+      params.require(:state).permit(:whats_needed, :primary, :absentee, :govenor, :govenor_phone, senator_attributes: [ :id, :name, :phone ])
     end
 end
