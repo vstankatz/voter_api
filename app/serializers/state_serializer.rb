e48ce14c8cdb3dded435bf8496c2fae3d494e1fa
Student <example@example.com>
2020-02-05 09:48:25 -0800

    Update serializers for new framework.

--- app/serializers/state_serializer.rb
+++ app/serializers/state_serializer.rb
@@ -1,5 +1,5 @@
 class StateSerializer < ActiveModel::Serializer
-  attributes :name, :code, :primary, :absentee, :whats_needed, :senators
-  has_one :current_politician, serializer: CurrentPoliticianSerializer
-  belongs_to :voter
+  attributes :id, :name, :code, :primary, :absentee, :whats_needed, :senators
+  has_many :senators, serializer: SenatorsSerializer
+  # belongs_to :voter
 end
