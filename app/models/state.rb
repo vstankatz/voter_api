8bedadbb5b930614788992f61f1a60ac0b501a49
Student <example@example.com>
2020-02-05 09:47:38 -0800

    Update models for new framework.

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,6 @@
 class State < ApplicationRecord
   validates_presence_of :whats_needed, :primary, :absentee, :name, :code, :voter_id
   belongs_to :voter, optional: true
-  has_one :current_politician, dependent: :destroy
-  accepts_nested_attributes_for :current_politician, :allow_destroy => true
+  has_many :senators, dependent: :destroy
+  accepts_nested_attributes_for :senators, :allow_destroy => true
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,8 @@
 class Voter < ApplicationRecord
   has_many :states, dependent: :destroy
   # accepts_nested_attributes_for :state_options
+  # has_one :current_politician
   validates_presence_of :general
   accepts_nested_attributes_for :states, :allow_destroy => true
+  # accepts_nested_attributes_for :current_politician, :allow_destroy => true
 end
