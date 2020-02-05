ee85f941e9bb44e3dfd969a39b1c9579869d42b9
Student <example@example.com>
2020-02-04 16:42:12 -0800

    Update voter and state serializers.

--- app/serializers/state_serializer.rb
+++ app/serializers/state_serializer.rb
@@ -1,4 +1,5 @@
 class StateSerializer < ActiveModel::Serializer
   attributes :name, :code, :primary, :absentee, :whats_needed, :senators
+  has_one :current_politician, serializer: CurrentPoliticianSerializer
   belongs_to :voter
 end
--- app/serializers/voter_serializer.rb
+++ app/serializers/voter_serializer.rb
@@ -1,5 +1,6 @@
 class VoterSerializer < ActiveModel::Serializer
   attributes :id, :general, :states
   has_many :states, serializer: StateSerializer
+  # has_one :current_politician, through: :states
 
 end
