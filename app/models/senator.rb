c47b04e649146ba52f93c591d48939147c568b2f
Student <example@example.com>
2020-02-05 13:38:09 -0800

    Update senator model.

--- app/models/senator.rb
+++ app/models/senator.rb
@@ -1,4 +1,4 @@
 class Senator < ApplicationRecord
-  validates_presence_of :name, :affiliation, :phone, :current_politician_id
+  validates_presence_of :name, :affiliation, :phone, :state_id
   belongs_to :states, optional: true
 end
