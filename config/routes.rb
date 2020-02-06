02cccf8c5f13df2ed23c16e94e92af6f874719da
Student <example@example.com>
2020-02-06 11:57:11 -0800

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :states
   resources :voters
-  resources :senators
+  # resources :senators
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
