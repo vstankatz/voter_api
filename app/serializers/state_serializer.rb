a2356c7f1129791c97ab63fdaa4d8dd421eb3b9e
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-07 14:59:04 -0800

    Add serialized id for access on front end.

--- app/serializers/state_serializer.rb
+++ app/serializers/state_serializer.rb
@@ -1,5 +1,5 @@
 class StateSerializer < ActiveModel::Serializer
-  attributes :name, :code, :govenor, :govenor_phone, :id_needed, :whats_needed, :primary, :absentee
+  attributes :id, :name, :code, :govenor, :govenor_phone, :id_needed, :whats_needed, :primary, :absentee
   has_many :senators, serializer: SenatorsSerializer
   has_many :elections, serializer: ElectionSerializer
   has_many :ballot_measures, serializer: BallotMeasureSerializer
